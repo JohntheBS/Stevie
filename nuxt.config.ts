91e052b294a949674e988658dd03fec8dd648107
johntheBS <tchindeyoukep@gmail.com>
2023-04-08 18:09:05 +0100

    packages install and configured

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@vueuse/nuxt",
+    "@nuxt/image",
+    "nuxt-headlessui",
+    "nuxt-icons",
+    "nuxt-swiper",
+  ],
 });
