fa4889640a21c24c29a4899c6a679a6014c79e7a
johntheBS <tchindeyoukep@gmail.com>
2023-04-08 17:00:34 +0100

    initial commit

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+<script setup lang="ts">
+let { data } = await useFetch(
+  "https://pizza-and-desserts.p.rapidapi.com/pizzas",
+  {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "c5dc4f699bmshdf8f4f664291b33p1cdc97jsnf4be1b75a2bf",
+      "X-RapidAPI-Host": "pizza-and-desserts.p.rapidapi.com",
+    },
+  }
+);
+</script>
