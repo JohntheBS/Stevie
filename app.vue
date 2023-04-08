cef0e5a0e3f0de1d627b964b9e0be8fe62ab984d
johntheBS <tchindeyoukep@gmail.com>
2023-04-08 20:03:54 +0100

    headless ui installed

--- app.vue
+++ app.vue
@@ -4,14 +4,14 @@
   </NuxtLayout>
 </template>
 <script setup lang="ts">
-let { data } = await useFetch(
-  "https://pizza-and-desserts.p.rapidapi.com/pizzas",
-  {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "c5dc4f699bmshdf8f4f664291b33p1cdc97jsnf4be1b75a2bf",
-      "X-RapidAPI-Host": "pizza-and-desserts.p.rapidapi.com",
-    },
-  }
-);
+// let { data } = await useFetch(
+//   "https://pizza-and-desserts.p.rapidapi.com/pizzas",
+//   {
+//     method: "GET",
+//     headers: {
+//       "X-RapidAPI-Key": "c5dc4f699bmshdf8f4f664291b33p1cdc97jsnf4be1b75a2bf",
+//       "X-RapidAPI-Host": "pizza-and-desserts.p.rapidapi.com",
+//     },
+//   }
+// );
 </script>
