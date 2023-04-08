97dbbdd085a8168dc1bd07c8d48c030ba15a7b77
johntheBS <tchindeyoukep@gmail.com>
2023-04-08 18:28:53 +0100

    slight deletes

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,7 @@
 <template>
   <div>
     <!-- header -->
-    <header class="bg-green-400 h-[450px]">
-      <nav class="flex justify-between">
-        <h1 class="text-3xl">logo</h1>
-        <ul class="flex gap-5">
-          <li>home</li>
-          <li>about</li>
-          <li>cuisine</li>
-          <li>menus</li>
-          <li>privatedining</li>
-        </ul>
-      </nav>
-    </header>
+    <header>default header</header>
     <slot />
     <!-- footer -->
     <footer>default footer</footer>
