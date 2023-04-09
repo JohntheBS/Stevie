8ca86bae61227956a496c5a9a89ce8fef0d6e5dc
johntheBS <tchindeyoukep@gmail.com>
2023-04-09 06:58:05 +0100

    ...

--- components/ScrollToTop.vue
+++ components/ScrollToTop.vue
@@ -1,7 +1,6 @@
 <template>
   <div
     class="p-3 btn-colors-secondary grid place-items-center rounded fixed bottom-6 right-6 shadow-[0px_0px_10px_-2px] shadow-white animate-bounce [animation-duration:_2s_ease-in] cursor-pointer"
-    @click="scrollToTop"
   >
     <nuxt-icon name="arrow-up" class="text-sm" />
   </div>
@@ -9,7 +8,7 @@
 
 <script setup lang="ts">
 const scrollToTop = () =>
-  document?.scrollingElement?.scrollTo({ behavior: "smooth", top: 0 });
+  // document?.scrollingElement?.scrollTo({ behavior: "smooth", top: 0 });
 
 // const html = ref<HTMLElement | null>(null);
 
