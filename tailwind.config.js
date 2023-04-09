f1882e474abbe9e798357059351b6a9bfb0ba122
Stephan-MC <mundestephane13@gmail.com>
2023-04-09 02:00:11 +0100

    Added breakpoints to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: [],
   theme: {
     extend: {
+      screens: {
+        mobile: '300px',
+        tablet: '481px',
+        laptop: '769px',
+        desktop: '1025px',
+        tv: '1280px',
+      },
       colors: {
         primary: "#f9690e",
         "primary-accent": "#222",
